c54e9fafb94b604e96ce51a4c78096b6808fe4a8
amit <90819609+amitballa94@users.noreply.github.com>
2021-09-17 09:55:50 +0530

    register changes

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -52,7 +52,7 @@
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
-                        <label for="">Last Namesdds</label>
+                        <label for="">Last Namesddsdds</label>
                         <input type="text"
                                class="form-control"  
                                name="lname" 
@@ -69,7 +69,7 @@
                 <div class="row">
                     <div class="col-sm-6">
                         <div class="form-group">
-                        <label for="">Emaildff</label>
+                        <label for="">Email maidsdd</label>
                         <input type="email" 
                                 class="form-control"
                                 name="email"
