40f0be2e8619ae9721004762c5016106e07c6d1a
amit <90819609+amitballa94@users.noreply.github.com>
2021-09-17 11:22:20 +0530

    mychange1

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -69,7 +69,7 @@
                 <div class="row">
                     <div class="col-sm-6">
                         <div class="form-group">
-                        <label for="">Email product</label>
+                        <label for="">Email productt</label>
                         <input type="email" 
                                 class="form-control"
                                 name="email"
